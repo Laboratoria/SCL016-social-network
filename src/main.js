0f50be9e707d79da813b3757e34011665a95b187
ana-225 <ana-225@gmail.com>
2021-04-27 18:30:37 -0400

    cambio de comillas a comillas simples en main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-const btnLogin = document.getElementById("btnMail")
-const loginDiv = document.getElementById("login");
-const sendRegisterBtn = document.getElementById("btnGo");
+const btnLogin = document.getElementById('btnMail')
+const loginDiv = document.getElementById('login');
+const sendRegisterBtn = document.getElementById('btnGo');
 const firebaseConfig = {
-   apiKey: "AIzaSyBMMBBn6lGE7PUp8zQNbs2hVvej5tVmVOo",
-   authDomain: "saudade-65a6b.firebaseapp.com",
-   projectId: "saudade-65a6b",
-   storageBucket: "saudade-65a6b.appspot.com",
-   messagingSenderId: "981149743720",
-   appId: "1:981149743720:web:34b1feb31b39e2c79399dd",
-   measurementId: "G-TCN7LTTV55"
+   apiKey: 'AIzaSyBMMBBn6lGE7PUp8zQNbs2hVvej5tVmVOo',
+   authDomain: 'saudade-65a6b.firebaseapp.com',
+   projectId: 'saudade-65a6b',
+   storageBucket: 'saudade-65a6b.appspot.com',
+   messagingSenderId: '981149743720',
+   appId: '1:981149743720:web:34b1feb31b39e2c79399dd',
+   measurementId: 'G-TCN7LTTV55'
  };
 firebase.initializeApp(firebaseConfig);
 const enviar = () => {
-  const email = document.getElementById("mailInput").value
-  const pass = document.getElementById("passInput").value
+  const email = document.getElementById('mailInput').value
+  const pass = document.getElementById('passInput').value
  firebase.auth().createUserWithEmailAndPassword(email, pass)
   .then((userCredential) => {
     var user = userCredential.user;
@@ -23,6 +23,6 @@ const enviar = () => {
     var errorMessage = error.message;
     alert(errorMessage)
   })}
- sendRegisterBtn.addEventListener("click", ()=> {
+ sendRegisterBtn.addEventListener('click', ()=> {
    enviar();
  })
