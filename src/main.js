6e0d84f5a9e3583a2be9a8815b521920d79376f1
ana-225 <ana-225@gmail.com>
2021-05-03 18:27:47 -0400

    se configura routing de login.js, profile.js y memorial.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 // import { register, access, authWithGoogle } from './lib/index.js';
-import { loginScreen } from './view/login.js'
+// import { loginScreen } from './view/login.js';
+import { changeRoute } from './lib/router.js';
 // CREACION DE PROYECTO EN EL SERVIDOR DE FIREBASE //
 const init = () => {
-const firebaseConfig = {
-  apiKey: 'AIzaSyBMMBBn6lGE7PUp8zQNbs2hVvej5tVmVOo',
-  authDomain: 'saudade-65a6b.firebaseapp.com',
-  projectId: 'saudade-65a6b',
-  storageBucket: 'saudade-65a6b.appspot.com',
-  messagingSenderId: '981149743720',
-  appId: '1:981149743720:web:34b1feb31b39e2c79399dd',
-  measurementId: 'G-TCN7LTTV55',
+  window.addEventListener('hashchange', () => {
+    changeRoute(window.location.hash);
+  });
+
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBMMBBn6lGE7PUp8zQNbs2hVvej5tVmVOo',
+    authDomain: 'saudade-65a6b.firebaseapp.com',
+    projectId: 'saudade-65a6b',
+    storageBucket: 'saudade-65a6b.appspot.com',
+    messagingSenderId: '981149743720',
+    appId: '1:981149743720:web:34b1feb31b39e2c79399dd',
+    measurementId: 'G-TCN7LTTV55',
+  };
+  firebase.initializeApp(firebaseConfig);
+  window.location.hash = '#/login';
 };
-firebase.initializeApp(firebaseConfig);
-if (window.location.hash == '#/login') {
-const divLogin = loginScreen();
-document.getElementById('rootLogin').appendChild(divLogin);
-} 
-}
-init();
+window.addEventListener('load', init);
